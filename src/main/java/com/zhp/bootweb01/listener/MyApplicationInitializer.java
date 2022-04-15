784dd7effe278dc25e16cf20ce28e9fd6e96b728
1641114339 <72444790+1641114339@users.noreply.github.com>
2022-04-15 14:48:59 +0800

    pull test

--- src/main/java/com/zhp/bootweb01/listener/MyApplicationInitializer.java
+++ src/main/java/com/zhp/bootweb01/listener/MyApplicationInitializer.java
@@ -7,9 +7,6 @@ public class MyApplicationInitializer implements ApplicationContextInitializer {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         System.out.println("我是初始化");
-        System.out.println("我是初始化2");
-        System.out.println("我是初始化3");
-        System.out.println("我是初始化4");
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("ssh push");
